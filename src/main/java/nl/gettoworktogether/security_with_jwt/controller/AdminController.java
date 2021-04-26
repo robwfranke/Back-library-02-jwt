85c7a0badca48d560280d9d6819c4728dc41500a
=Rob Franke <robwfranke@gmail.com>
2021-04-26 10:24:23 +0200

    start

---
+++ src/main/java/nl/gettoworktogether/security_with_jwt/controller/AdminController.java
@@ -0,0 +1,18 @@
+package nl.gettoworktogether.security_with_jwt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @GetMapping(value = "")
+    public ResponseEntity<Object> getMessage() {
+        return new ResponseEntity<>("SECURED REST endpoint: /admin", HttpStatus.OK);
+    }
+
+}
